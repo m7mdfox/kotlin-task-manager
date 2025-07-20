56b878e5aac92bf3dd24d986c133468e3c9b4afa
m7mdfox <fcb.mohamed2005@gmail.com>
2025-07-21 01:40:42 +0300

    Final code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,32 @@
 package org.example
 
 fun main() {
+    // list containing all tasks and it is mutable to add in it
+    val list = mutableListOf<Task>()
+    list.add(Task("Submit assignment",true))
+    list.add(Task("Clean House"))
+    list.add(Task("Watch recorded videos",true))
+    println("All Tasks are: ")
+    for (item in list) {
+        if (item.isDone){
+            print("✅")
+        }else{
+            print("❌")
+        }
+        println(" ${item.title}")
+    }
+    println("===================================")
+    println("Not done tasks are: ")
+    // for loop to display unfinished tasks
+    for (item in list.filter { !it.isDone }) {
+        println("-${item.title}")
+    }
+    println("===================================")
+    // loop for finished tasks
+    println("Done tasks are: ")
+    for (item in list.filter { it.isDone }) {
+        println("-${item.title}")
+    }
 
 }
 
